41f1cde168871fbae9a4714d98d81a9fb3da9d2f
John <salacjp0@gmail.com>
2024-05-14 17:32:53 -0800

    create category card

--- wp-content/themes/understrap-child-main/front-page.php
+++ wp-content/themes/understrap-child-main/front-page.php
@@ -1,11 +1,125 @@
 <?php
 get_header();
-?>
-
-<div class="container">
-
+?><div class="container mb-5">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://placehold.co/600x400" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://placehold.co/600x400" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://placehold.co/600x400" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 </div>
 
+<div class="container ">
+    <div class="row-cols-md-4 row-cols-2 row gy-2  gx-2 p-3  justify-content-center">
+        <div class="col">
+            <div class="card">
+                <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <!-- <h5 class="card-title">Special title treatment</h5> -->
+                    <p class="card-text">add text here.</p>
+                    <a href="#" class="btn btn-primary">Add to Cart 1</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <!-- <h5 class="card-title">Special title treatment</h5> -->
+                    <p class="card-text">add text here.</p>
+                    <a href="#" class="btn btn-primary">Add to Cart 2</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <!-- <h5 class="card-title">Special title treatment</h5> -->
+                    <p class="card-text">add text here.</p>
+                    <a href="#" class="btn btn-primary">Add to Cart 3</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <!-- <h5 class="card-title">Special title treatment</h5> -->
+                    <p class="card-text">add text here.</p>
+                    <a href="#" class="btn btn-primary">Add to Cart 4</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <!-- <h5 class="card-title">Special title treatment</h5> -->
+                    <p class="card-text">add text here.</p>
+                    <a href="#" class="btn btn-primary">Add to Cart 5</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <!-- <h5 class="card-title">Special title treatment</h5> -->
+                    <p class="card-text">add text here.</p>
+                    <a href="#" class="btn btn-primary">Add to Cart 6</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <!-- <h5 class="card-title">Special title treatment</h5> -->
+                    <p class="card-text">add text here .</p>
+                    <a href="#" class="btn btn-primary">Add to Cart 7</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <!-- <h5 class="card-title">Special title treatment</h5> -->
+                    <p class="card-text">add text here.</p>
+                    <a href="#" class="btn btn-primary">Add to Cart 8</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
 <?php
 get_footer();
 ?>
+<!-- row-cols-auto 
+row-gx-5 -sm-3 
+
+row gy-2 gx-2 justify-content-center -->
